9b208ca4933eba440a50e1fbc856a0466d6f3200
eddie-salas1 <knowledge980@yahoo.com>
2024-07-31 10:25:38 -0700

    Even more loops

---
+++ loops4.m
@@ -0,0 +1,8 @@
+% More fun with loops
+clc, clearvars
+% Executes code 50 times, when for loop iX runs once iY runs 5 times before end and incrementing
+for iX = 1:10
+     for iY=1:5
+        iX+iY
+     end
+end
